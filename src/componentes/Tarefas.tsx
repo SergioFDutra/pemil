a5869dcb8ffe177fe529c10960397cddc41e7886
SergioFernandesDutra <engenheirosergiodutra@gmail.com>
2023-02-23 10:47:32 -0300

    Função editar definida

--- src/componentes/Tarefas.tsx
+++ src/componentes/Tarefas.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, FormEvent } from 'react'
 import { supabase } from '../../lib/initSupabase';
 
+
 interface Tarefa {
     id?: number;
     descricao: string
@@ -13,6 +14,8 @@ export default function Tarefas() {
     const [descricao, setDescricao] = useState<string>();
 
     const [tarefas, setTarefas] = useState([]);
+
+    const [selectedStatus, setSelectedStatus] = useState('');
 
     useEffect(() => {
         buscaTarefas()
@@ -60,7 +63,9 @@ export default function Tarefas() {
 
         await supabase
             .from('tarefas')
-            .update({ descricao: 'textEditar.value' })
+            .update([
+                { descricao: descricao },
+            ])
             .eq('id', id)
     }
 
@@ -74,12 +79,21 @@ export default function Tarefas() {
         setTarefas(tarefas.filter((x) => x.id != id))
     }
 
-    const selecionaStatus = async (status: boolean) => {
+    const selecionaStatus = async (status: any) => {
         console.log("Selecionando status", status);
         let { data: tarefas, error } = await supabase
             .from('tarefas')
             .select('status')
         if (error) console.log('error', error)
+
+        if ([{ status: 'true' }]) {
+
+
+        }else if([{ status: 'false' }]){
+
+        }else if([{ status: 'todas' }]){
+
+        }
 
     }
 
@@ -110,10 +124,8 @@ export default function Tarefas() {
                                         setDescricao(e.target.value)
                                     }}
                                 />
-                                <button onClick={() => editarTarefa(tarefa.id)}>
-                                    Editar
-                                </button>
-                                <button onClick={() => removerTarefa(tarefa.id)}>Remover</button>
+                                <button className='new-task-button' onClick={() => editarTarefa(tarefa.id)}>Editar</button>
+                                <button className='new-task-button' onClick={() => removerTarefa(tarefa.id)}>Remover</button>
                             </li>
                         ))}
                     </ol>
@@ -123,20 +135,34 @@ export default function Tarefas() {
 
             <div className="container">
 
-
                 <form className="new-task-container" onSubmit={selecionaStatus}>
-                    <div className="label">Tarefa Realizada</div>
-                    <input type="checkbox" id="txtPesquisa" />
-                    <button className="new-task-button" type='submit'>Pesquisar</button>
+                    <label> Escolha o tipo de tarefa:
+                        <select onChange={(e) => { setDescricao(e.target.value) }}>
+                            <option value="">Selecione</option>
+                            <option value="true">Tarefas realizadas</option>
+                            <option value="false">Tarefas não realizadas</option>
+                            <option value="todas">Todas</option>
+                        </select>
+                    </label>
+                    <button className="new-task-button" /*type='submit' value="Enviar"*/ onClick={() => selecionaStatus(tarefa.id)} >Pesquisar</button>
                 </form>
+
+
+
+                {/* <form className="new-task-container" onSubmit={selecionaStatus}>
+                    <div className="label">Escolha o tipo de tarefa...</div>
+                    <select placeholder='Escolha o tipo de tarefa...'>
+                        <option value="realizada">Tarefas realizadas</option>
+                        <option value="naoRealizada">Tarefas não realizadas</option>
+                        <option selected value="todas">Todas</option>
+                    </select>
+                    <button className="new-task-button" type='submit'>Pesquisar</button>
+                </form> */}
                 <div className="tasks-container">
                     <ol className="items-grid">
                         {tarefas?.map(tarefa => (
-                            <li key={tarefa.id}>
+                            <li className='lista' key={tarefa.id}>
                                 <span>{tarefa.descricao}</span>
-                                {/* <button className="btn-black" onClick={() => editarTarefa(tarefa)}>
-                                    Editar
-                                </button> */}
                             </li>
                         ))}
                     </ol>
